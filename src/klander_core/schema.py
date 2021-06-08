4e07569947f59de1d6eb3b71e7ca0e9c738cfd13
David Delassus <david.jose.delassus@gmail.com>
2021-06-08 13:07:42 +0200

    :recycle: use dict() syntax instead of {} for readability

--- src/klander_core/schema.py
+++ src/klander_core/schema.py
@@ -5,131 +5,131 @@ JSON schema for data validation.
 from .matcher import match_operators
 
 
-state_reconciler = {
-    'type': 'object',
-    'required': ['apiVersion', 'kind', 'metadata', 'spec'],
-    'definitions': {
-        'observer': {
-            'type': 'object',
-            'required': ['apiVersion', 'kind', 'namespaces'],
-            'properties': {
-                'apiVersion': {'type': 'string'},
-                'kind': {'type': 'string'},
-                'namespaces': {
-                    'oneOf': [
-                        {'type': 'string', 'enum': ['*']},
-                        {'type': 'array', 'items': {'type': 'string'}}
+state_reconciler = dict(
+    type='object',
+    required=['apiVersion', 'kind', 'metadata', 'spec'],
+    definitions=dict(
+        observer=dict(
+            type='object',
+            required=['apiVersion', 'kind', 'namespaces'],
+            properties=dict(
+                apiVersion=dict(type='string'),
+                kind=dict(type='string'),
+                namespaces=dict(
+                    oneOf=[
+                        dict(type='string', enum=['*']),
+                        dict(type='array', items=dict(type='string'))
                     ]
-                }
-            }
-        },
-        'matcher': {
-            'type': 'object',
-            'oneOf': [
-                {
-                    'required': ['field', 'where'],
-                    'properties': {
-                        'field': {'type': 'string'},
-                        'where': {
-                            'type': 'array',
-                            'items': [
-                                {
-                                    'type': 'string',
-                                    'enum': list(match_operators.keys())
-                                },
-                                {}
+                )
+            )
+        ),
+        matcher=dict(
+            type='object',
+            oneOf=[
+                dict(
+                    required=['field', 'where'],
+                    properties=dict(
+                        field=dict(type='string'),
+                        where=dict(
+                            type='array',
+                            items=[
+                                dict(
+                                    type='string',
+                                    enum=list(match_operators.keys())
+                                ),
+                                dict()
                             ]
-                        }
-                    }
-                },
-                {
-                    'required': ['oneOf'],
-                    'properties': {
-                        'oneOf': {
-                            'type': 'array',
-                            'items': {'$ref': '#/definitions/matcher'}
-                        }
-                    }
-                },
-                {
-                    'required': ['allOf'],
-                    'properties': {
-                        'allOf': {
-                            'type': 'array',
-                            'items': {'$ref', '#/definitions/matcher'}
-                        }
-                    }
-                }
+                        )
+                    )
+                ),
+                dict(
+                    required=['oneOf'],
+                    properties=dict(
+                        oneOf=dict(
+                            type='array',
+                            items={'$ref': '#/definitions/matcher'}
+                        )
+                    )
+                ),
+                dict(
+                    required=['allOf'],
+                    properties=dict(
+                        allOf=dict(
+                            type='array',
+                            items={'$ref': '#/definitions/matcher'}
+                        )
+                    )
+                )
             ]
-        },
-        'reconciler': {
-            'type': 'object',
-            'oneOf': [
-                {
-                    'required': ['deleteExtras'],
-                    'properties': {
-                        'deleteExtras': {'type': 'boolean'}
-                    }
-                },
-                {
-                    'required': ['patch'],
-                    'properties': {
-                        'patch': {'type': 'object'}
-                    }
-                },
-                {
-                    'required': ['webhook'],
-                    'properties': {
-                        'webhook': {'type': 'string', 'format': 'url'}
-                    }
-                }
+        ),
+        reconciler=dict(
+            type='object',
+            oneOf=[
+                dict(
+                    required=['deleteExtras'],
+                    properties=dict(
+                        deleteExtras=dict(type='boolean')
+                    )
+                ),
+                dict(
+                    required=['patch'],
+                    properties=dict(
+                        patch=dict(type='object')
+                    )
+                ),
+                dict(
+                    required=['webhook'],
+                    properties=dict(
+                        webhook=dict(type='string', format='url')
+                    )
+                )
             ]
-        }
-    },
-    'properties': {
-        'apiVersion': {'type': 'string', 'enum': ['datapio.co/v1']},
-        'kind': {'type': 'string', 'enum': ['StateReconciler']},
-        'metadata': {
-            'type': 'object',
-            'required': ['name'],
-            'properties': {
-                'name': {'type': 'string'}
-            }
-        },
-        'spec': {
-            'type': 'object',
-            'required': ['observe', 'match', 'reconcile'],
-            'properties': {
-                'observe': {'$ref': '#/definitions/observer'},
-                'match': {'$ref': '#/definitions/matcher'},
-                'reconcile': {'$ref': '#/definitions/reconciler'}
-            }
-        }
-    }
-}
+        )
+    ),
+    properties=dict(
+        apiVersion=dict(type='string', enum=['datapio.co/v1']),
+        kind=dict(type='string', enum=['StateReconciler']),
+        metadata=dict(
+            type='object',
+            required=['name'],
+            properties=dict(
+                name=dict(type='string')
+            )
+        ),
+        spec=dict(
+            type='object',
+            required=['observe', 'match', 'reconcile'],
+            properties=dict(
+                observe={'$ref': '#/definitions/observer'},
+                match={'$ref': '#/definitions/matcher'},
+                reconcile={'$ref': '#/definitions/reconciler'}
+            )
+        )
+    )
+)
 
-state_reconciliation_response = {
-    'type': 'object',
-    'required': ['apiVersion', 'kind', 'spec'],
-    'properties': {
-        'apiVersion': {'type': 'string', 'enum': ['datapio.co/v1']},
-        'kind': {'type': 'string', 'enum': ['StateReconciliationResponse']},
-        'spec': {
-            'type': 'object',
-            'oneOf': [
-                {
-                    'required': ['delete'],
-                    'properties': {
-                        'delete': {'type': 'boolean'}
-                    }
-                },
-                {
-                    'required': ['patch'],
-                    'properties': {
-                        'patch': {'type': 'object'}
-                    }
-                }
+state_reconciliation_response = dict(
+    type='object',
+    required=['apiVersion', 'kind', 'spec'],
+    properties=dict(
+        apiVersion=dict(type='string', enum=['datapio.co/v1']),
+        kind=dict(type='string', enum=['StateReconciliationResponse']),
+        spec=dict(
+            type='object',
+            oneOf=[
+                dict(
+                    required=['delete'],
+                    properties=dict(
+                        delete=dict(type='boolean')
+                    )
+                ),
+                dict(
+                    required=['patch'],
+                    properties=dict(
+                        patch=dict(type='object')
+                    )
+                )
             ]
-        }
-    }
-}
+        )
+    )
+)
